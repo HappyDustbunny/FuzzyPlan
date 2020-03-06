45ff342f377ce8f3543544a4b5d46e0cad168172
Mads Horn <madshorn@operamail.com>
2020-03-06 17:19:38 +0100

    Yet another major refactoring

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var taskList = [];
 let chosenTaskId = '';
+blockIdList = [117, 9030, 2979, 7649, 700, 3099, 1582, 4392, 3880, 5674, 8862, 5220, 9349, 6299, 1367, 4317, 9225, 1798, 7571, 4609, 6907, 1194, 9487, 9221, 2763, 1553, 128, 1318, 8762, 4974, 6508, 5277, 8256, 3863, 2860, 1904, 1218, 3932, 3615, 7110, 6770, 9075, 5270, 9184, 2702, 1039, 3420, 8488, 5522, 6071, 7870, 740, 2866, 8387, 3628, 5684, 9356, 6843, 9239, 9137, 9114, 5203, 8243, 9374, 9505, 9351, 7053, 4414, 8847, 5835, 9669, 9216, 7724, 5834, 9295, 1948, 8617, 9822, 5452, 2651, 5616, 4355, 1910, 2591, 8171, 7415, 7456, 2431, 4051, 4552, 9965, 7528, 911, 734, 6896, 249, 7375, 1035, 8613, 8836];
 
-function Task(date, duration, text) {
+function Task(date, duration, text, blockId) {
   this.date = date; // Time as Javascript date
   this.duration = duration; // Duration in milliseconds
   this.text = text;
+  this.blockId = blockId;
 
   this.height = function() { // Pixelheight is 1 minute = 1 px
     return this.duration / 60000
@@ -13,10 +15,8 @@ function Task(date, duration, text) {
   this.fuzzyness = function() {
     if (this.text == '') {
       return 'isNullTime'
-    } else if (this.duration == '0' &&  (this.date == '')) { // No starttime or duration
+    } else if (this.date == '') { // No starttime
       return 'isFuzzy'
-    } else if (this.duration != '0' &&  (this.date == '')) { // No starttime, but duration
-      return 'isFuzzyish'
     } else {
       return 'isNotFuzzy'
     }
@@ -27,16 +27,15 @@ function setUpFunc() {
   // Create 24h nullTime
   let now = new Date();
   let fullNullStart = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 1);
-  let day24h = 24 * 3600 * 1000;
-  let startNullTime = new Task(fullNullStart, day24h, '');
+  let day24h = 24 * 3600 * 1000;  // Milliseconds in a day
+  let startNullTime = new Task(fullNullStart, day24h, '', blockIdList.pop());
   taskList.push(startNullTime);
 
   // Create a 15 minute planning time as a starting point
-  let planningTask = new Task(now, 15 * 60000, 'Planning');
-  insertFixTimeTask(planningTask);
+  // let planningTask = new Task(now, 15 * 60000, 'Planning', startNullTime.blockId);
+  insertFixTimeTask([now, 15 * 60000, 'Planning']);
 
-  // taskList.forEach(renderTasks) // Draws task based on the content of the taskList
-  renderTasks();
+  renderTasks();  // Draws task based on the content of the taskList
   resetInputBox();
 }
 
@@ -58,51 +57,76 @@ inputBox.addEventListener('keypress', function (e) {
 function addTask() { // TODO: Make more like insertFixTimeTask
   let contentInputBox = document.getElementById('inputBox').value.trim();
   let parsedList = parseText(contentInputBox);
-  let newTask = new Task(parsedList[0], parsedList[1], parsedList[2]);
-  if (newTask.fuzzyness() == 'isNotFuzzy') {
-    insertFixTimeTask(newTask);
+  if (parsedList[0] == '') {  // No fixed time ...
+    insertTask(parsedList);
   } else {
-    insertTask(newTask);
+    insertFixTimeTask(parsedList);
   }
 
   if (editButton.dataset.clonemode === 'false') {
     resetInputBox();
   }
 }
+// TODO: Add task duration to start time for nullTime
+function insertTask(parsedList) {
+  let newTaskDuration = parsedList[1];
 
-function insertTask(newTask) {
-  let newTaskDuration = newTask.duration;
-  taskList.forEach((task, index) => {  // Find the first nullTime slot
-    if (newTask.duration == 0) {
-      newTaskDuration = 30 * 60000;
-    }
+  for (const [index, task] of taskList.entries()) {
+      // Find the first nullTime slot
+      if (task.fuzzyness() == 'isNullTime' && newTaskDuration < task.duration && index > 0) {
+        taskList[index].date = new Date(task.date.getTime() + newTaskDuration);  // ..then shrink the nullTime
+        task.duration -= newTaskDuration;
+        // Make the new task ...
+        let newTask = new Task(parsedList[0], parsedList[1], parsedList[2], task.blockId);
+        //  And insert the new task before chosen nullTime
+        taskList.splice(index, 0, newTask);
+        break
+      }
+  }
 
-    if (task.fuzzyness() == 'isNullTime' && newTask.duration < task.duration && index > 0) {
-      nullTimeDuration = task.duration - newTask.duration;
-      newTime = new Date(task.date.getTime() + newTaskDuration);
-      let nullTimeTask = new Task(newTime, nullTimeDuration, '');
-
-      taskList.splice(index, 1, newTask, nullTimeTask);
-    }
-  });
+  // taskList.forEach((task, index) => {  // Find the first nullTime slot
+  //   if (task.fuzzyness() == 'isNullTime' && newTask.duration < task.duration && index > 0) {
+  //     newTime = new Date(task.date.getTime() + newTaskDuration);  // ..then shrink the nullTime
+  //     nullTimeDuration = task.duration - newTaskDuration;
+  //     let nullTimeTask = new Task(newTime, nullTimeDuration, '');
+  //
+  //     taskList.splice(index, 1, newTask, nullTimeTask);  //  Delete old nullTime and insert new task and new nullTime
+  //   }
+  // });
 }
 
-function insertFixTimeTask(fixTimeTask) {
-  taskList.forEach((item, i) => {
-    if (item.fuzzyness() == 'isNullTime') { // Find first nullTime slot
-      let nullTimeEnd = new Date(item.date.getTime() + item.duration);
-      if ((item.date <= fixTimeTask.date) && (fixTimeTask.duration < item.duration) && (fixTimeTask.date < nullTimeEnd)) {
-        null1Duration = fixTimeTask.date - item.date;
-        let null1 = new Task(item.date, null1Duration, '')
+function insertFixTimeTask(parsedList) {
+  let succes = false;
+  for (const [index, task] of taskList.entries()) {
+    if (task.fuzzyness() == 'isNullTime') { // Find first nullTime slot
+      let newFixedTimeTask = new Task(parsedList[0], parsedList[1], parsedList[2], task.blockId);
+      let nullTimeEnd = new Date(task.date.getTime() + task.duration);
+      if ((task.date <= newFixedTimeTask.date) && (newFixedTimeTask.duration < task.duration) && (newFixedTimeTask.date < nullTimeEnd)) {
+        null1Duration = newFixedTimeTask.date - task.date;
+        let null1 = new Task(task.date, null1Duration, '', task.blockId)
 
-        null2Duration = item.date.getTime() + item.duration - fixTimeTask.date.getTime() - fixTimeTask.duration;
-        let endTime = new Date(fixTimeTask.date.getTime() + fixTimeTask.duration);
-        let null2 = new Task(endTime, null2Duration, '');
 
-        taskList.splice(i, 1, null1, fixTimeTask, null2);
+        null2Duration = task.date.getTime() + task.duration - newFixedTimeTask.date.getTime() - newFixedTimeTask.duration;
+        let endTime = new Date(newFixedTimeTask.date.getTime() + newFixedTimeTask.duration);
+        let null2 = new Task(endTime, null2Duration, '', blockIdList.pop());
+
+        taskList.splice(index, 1, null1, newFixedTimeTask, null2);
+        succes = true;
       }
     }
-  });
+  }
+  if (!succes) {
+    editButton.dataset.clonemode = 'true' // If there isn't enough room for a fixTimeTask flash a waring
+    msg = document.getElementById('message');
+    msg.width = '300px';
+    msg.style.display = 'inline-block';
+    msg.style.textAlign = 'center';
+    msg.style.color = 'red';
+    msg.innerText = 'Not enough room\nPlease clear some space';
+
+    setTimeout(function() {msg.style.display = 'none';}, 3000)
+  }
+  // });
 }
 
 function renderTasks() {
@@ -115,13 +139,14 @@ function renderTasks() {
     let newNode = document.createElement('div');
     newNode.setAttribute('id', index);
     newNode.classList.add(task.fuzzyness());
-    newNode.setAttribute('onClick', 'taskHasBeenClicked(this.id)');  // TODO: addEventListener here?
-    if (task.fuzzyness() == 'isFuzzy' || index == 0) {
+    if (index == 0) { // Collapse the nullTime block before the planning task as it can't be interacted with
       newNode.style['line-height'] = '30px';
       newNode.style.height = '30px';
+      newNode.style.background = '#e3ebf2';  // Make non-interactive nullTime block grey
     } else {
       newNode.style.height = task.height() + 'px';
       newNode.style['line-height'] = task.height() + 'px';
+      newNode.setAttribute('onClick', 'taskHasBeenClicked(this.id)');  // TODO: addEventListener here?
     }
 
     let nodeText = textExtractor(task);
@@ -130,6 +155,7 @@ function renderTasks() {
     document.getElementById('day').insertAdjacentElement('beforeend', newNode);
   });
 }
+
 // TODO: textExtractor and renderTasks should add time to not-fixTimeTasks
 function textExtractor(task) {
   let text = task.text;
@@ -227,6 +253,9 @@ function parseText(rawText) {
 
   // Make duration in milliseconds form hours and minutes
   let duration = hours * 3600000 + minutes * 60000;
+  if (duration == 0) {
+    duration = 30 * 60000; // If no duration is provided the task is assigned 30 minutes as default
+  }
 
   let time = /[0-9]?[0-9]:?[0-9][0-9]/.exec(rawText);
   if (time) { // If 1230 or 12:30 is in rawText store numbers in hours and minutes and remove 1230 from rawText
