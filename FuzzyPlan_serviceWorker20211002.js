823491b913aec5a0c93cccc4f98e8c7a98024195
Mads Horn <madshorn@operamail.com>
2023-05-08 13:13:56 +0200

    Fix undo-button not being inactive outside dayView

--- FuzzyPlan_serviceWorker20211002.js
+++ FuzzyPlan_serviceWorker20211002.js
@@ -1,5 +1,5 @@
 
-let VERSION = 1.003; // Change version number to facilitate updates
+let VERSION = 1.005; // Change version number to facilitate updates
 
 let FP_CACHE = 'FP-cache';
 let CACHED_URLS = [
